30115f0ae44ec28572c2f9c176cb403477642cf8
nellizi <masterliz9095@gmail.com>
2023-07-26 13:20:31 +0900

    Refactor : Frame 완성

--- src/everything/inzent/ecm/frame/Frame.java
+++ src/everything/inzent/ecm/frame/Frame.java
@@ -25,35 +25,30 @@ import javax.swing.table.TableColumnModel;
 
 import everything.inzent.ecm.file.MyFile;
 
-
 public class Frame extends JFrame {
 
 	/*
-	 * 1. new Frame() 하면 상단 패널만 존재하는 frame 생성하기 
-	 * 2. 상단 패널에 디렉토리 입력 텍스트필드 추가 
-	 * 3. 검색버튼추가
-	 * 4. 검색버튼 추가시 하단 패널 띄우기 
-	 *  4-1. 하단 패널 띄울 때 2번의 텍스트필드 값 매개변수로 넘기기 
-	 * 5. 매개변수로 받은 값파일 불러오는 매서드 매개변수로 넘기기
+	 * 1. new Frame() 하면 상단 패널만 존재하는 frame 생성하기 2. 상단 패널에 디렉토리 입력 텍스트필드 추가 3. 검색버튼추가
+	 * 4. 검색버튼 추가시 하단 패널 띄우기 4-1. 하단 패널 띄울 때 2번의 텍스트필드 값 매개변수로 넘기기 5. 매개변수로 받은 값파일
+	 * 불러오는 매서드 매개변수로 넘기기
 	 */
 
 	private JFrame frame;
-	private JTextField textField;
-	private JTextField textField_1;
 	private JTextField textField_2;
 	private JPanel tablePanel;
-	private String dirPath ="";
+	private String dirPath = "";
 	private MyFile file;
 
 	public Frame() {
 		super("EveryThing_By_Liz");
-		
+
 		file = new MyFile();
 //		frame = new JFrame("EveryThing_By_Liz");
-		super.setSize(881, 613);
+		super.setSize(1027, 689);
 
 		JPanel panelTop = new JPanel();
-		panelTop.setBounds(6, 500, 990, 59);
+		getContentPane().add(panelTop);
+		panelTop.setBounds(6, 10, 850, 50);
 		SpringLayout sl_panelTop = new SpringLayout();
 		panelTop.setLayout(sl_panelTop);
 
@@ -66,50 +61,50 @@ public class Frame extends JFrame {
 		sl_panelTop.putConstraint(SpringLayout.EAST, address, -6, SpringLayout.WEST, btn1);
 		sl_panelTop.putConstraint(SpringLayout.EAST, btn1, -32, SpringLayout.EAST, panelTop);
 		panelTop.add(btn1);
-		getContentPane().add(panelTop);
-
-		
 		JPanel bottomPanel = new JPanel();
-		sl_panelTop.putConstraint(SpringLayout.NORTH, bottomPanel, -522, SpringLayout.SOUTH, panelTop);
-		sl_panelTop.putConstraint(SpringLayout.WEST, bottomPanel, 10, SpringLayout.WEST, panelTop);
-		sl_panelTop.putConstraint(SpringLayout.SOUTH, bottomPanel, -10, SpringLayout.SOUTH, panelTop);
-		sl_panelTop.putConstraint(SpringLayout.EAST, bottomPanel, 855, SpringLayout.WEST, panelTop);
-		panelTop.add(bottomPanel);
+		bottomPanel.setBounds(6, 70, 850, 500);
+		getContentPane().add(bottomPanel);
 		SpringLayout sl_panel = new SpringLayout();
 		bottomPanel.setLayout(sl_panel);
 		bottomPanel.setVisible(false);
 
-		
 		textField_2 = new JTextField(20);
-		sl_panel.putConstraint(SpringLayout.NORTH, textField_2, 6, SpringLayout.NORTH, bottomPanel);
-		sl_panel.putConstraint(SpringLayout.WEST, textField_2, 21, SpringLayout.WEST, bottomPanel);
-		sl_panel.putConstraint(SpringLayout.EAST, textField_2, -84, SpringLayout.EAST, bottomPanel);
+		sl_panel.putConstraint(SpringLayout.WEST, textField_2, 39, SpringLayout.WEST, bottomPanel);
 		bottomPanel.add(textField_2);
 
 		JButton btn1_1 = new JButton("검색");
-		sl_panel.putConstraint(SpringLayout.NORTH, btn1_1, -1, SpringLayout.NORTH, textField_2);
-		sl_panel.putConstraint(SpringLayout.WEST, btn1_1, 6, SpringLayout.EAST, textField_2);
+		sl_panel.putConstraint(SpringLayout.EAST, textField_2, -6, SpringLayout.WEST, btn1_1);
+		sl_panel.putConstraint(SpringLayout.WEST, btn1_1, 768, SpringLayout.WEST, bottomPanel);
+		sl_panel.putConstraint(SpringLayout.NORTH, btn1_1, 65, SpringLayout.NORTH, bottomPanel);
+		sl_panel.putConstraint(SpringLayout.NORTH, textField_2, 1, SpringLayout.NORTH, btn1_1);
 		bottomPanel.add(btn1_1);
-		
-		
 
 		tablePanel = new JPanel();
-		sl_panel.putConstraint(SpringLayout.NORTH, tablePanel, 5, SpringLayout.SOUTH, btn1_1);
-		sl_panel.putConstraint(SpringLayout.WEST, tablePanel, 21, SpringLayout.WEST, bottomPanel);
-		sl_panel.putConstraint(SpringLayout.SOUTH, tablePanel, 474, SpringLayout.SOUTH, btn1_1);
-		sl_panel.putConstraint(SpringLayout.EAST, tablePanel, 823, SpringLayout.WEST, bottomPanel);
+		sl_panel.putConstraint(SpringLayout.NORTH, tablePanel, 112, SpringLayout.NORTH, bottomPanel);
+		sl_panel.putConstraint(SpringLayout.EAST, tablePanel, -34, SpringLayout.EAST, bottomPanel);
+		sl_panel.putConstraint(SpringLayout.WEST, tablePanel, 39, SpringLayout.WEST, bottomPanel);
+		sl_panel.putConstraint(SpringLayout.SOUTH, tablePanel, -10, SpringLayout.SOUTH, bottomPanel);
 		bottomPanel.add(tablePanel);
 
-		JPanel panelbottom = new JPanel();
-		panelbottom.setBounds(200, 500, 990, 59);
+		// 이벤트? 실행되는 시점을 알려주기 위함
+		btn1.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				dirPath = address.getText();
+				bottomPanel.setVisible(true);
+				showTable(dirPath);
+			}
+		});
 
-		
-		JTextField filePath = new JTextField(20);
-		panelbottom.add(filePath);
-		JButton btn2 = new JButton("검색2");
-		panelbottom.add(btn2);
-		
-		
+//		JPanel panelbottom = new JPanel();
+//		panelbottom.setBounds(200, 500, 990, 59);
+//
+//		
+//		JTextField filePath = new JTextField(20);
+//		panelbottom.add(filePath);
+//		JButton btn2 = new JButton("검색2");
+//		panelbottom.add(btn2);
+//		
 
 		Dimension frameSize = getSize();
 		// 해상도 값
@@ -117,22 +112,11 @@ public class Frame extends JFrame {
 		setLocation((windowSize.width - frameSize.width) / 2, (windowSize.height - frameSize.height) / 2); // 화면 중앙에 띄우기
 		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		setVisible(true);
-		
-		showTable(dirPath);
-
-		// 이벤트? 실행되는 시점을 알려주기 위함
-		btn1.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				dirPath = address.getText(); 
-				bottomPanel.setVisible(true);
-			}
-		});
 
 	}
 
 	public void showTable(String address) {
-		
+
 		String[][] data = file.getList(address);
 
 		String[] header = new String[] { "이름", "경로", "크기", "수정한 날짜" };
@@ -178,5 +162,5 @@ public class Frame extends JFrame {
 		tablePanel.add(scrollPane); // JScrollPane을 panelTop에 바로 올리면 안 보임. 전용 tablePanel에 올려야 보임
 
 	}
-	
+
 }
